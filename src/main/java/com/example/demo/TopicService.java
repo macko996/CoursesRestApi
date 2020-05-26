62b40947a537efda319ed8fd178262b170f6e14c
Marjan Kolevski <Marjan.Kolevski@netcetera.com>
2020-05-26 13:17:03 +0200

    Added POST

--- src/main/java/com/example/demo/TopicService.java
+++ src/main/java/com/example/demo/TopicService.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,11 +11,11 @@ import static java.util.Arrays.*;
 @Service
 public class TopicService {
 
-    private List<Topic> topics = asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic ("spring" ,"spring framework", "spring framework description"),
                 new Topic ("java" ,"java core", "java description"),
                 new Topic ("javascript" ,"JavaScript", "javascript framework description")
-        );
+    ));
 
     public List<Topic> getAllTopics(){
         return topics;
@@ -23,5 +24,9 @@ public class TopicService {
     public Topic getTopic(String id){
 
         return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }
