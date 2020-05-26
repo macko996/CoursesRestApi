9a156b8e4bbfaa24d9fbb193aec95c4fb21cfa91
Marjan Kolevski <Marjan.Kolevski@netcetera.com>
2020-05-26 12:57:32 +0200

    Initial commit :GET finished

---
+++ src/main/java/com/example/demo/TopicService.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.*;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = asList(
+            new Topic ("spring" ,"spring framework", "spring framework description"),
+                new Topic ("java" ,"java core", "java description"),
+                new Topic ("javascript" ,"JavaScript", "javascript framework description")
+        );
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+
+    public Topic getTopic(String id){
+
+        return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+    }
+}
